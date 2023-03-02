83650ec32f152f079ca3c146a99c6799cdf54206
PierreAlexisDaCosta <pierrealexis.da.costa@gmail.com>
2023-03-02 12:35:33 +0100

    Git test

--- Script/Packages.R
+++ Script/Packages.R
@@ -1,5 +1,4 @@
 # Load packages ####
-
 library(tidyverse)
 library(dplyr)
 library(tibble)
@@ -23,11 +22,16 @@ library(VIM)
 
 library(gtsummary)
 
-
-
-
 # github configuration ####
 library(usethis)
+library(gitcreds)
 usethis::edit_git_config() #Veryfy username and mail
 
 usethis::use_git()
+usethis::create_github_token()
+#This will take you to the appropriate page on the GitHub website, 
+#where you’ll give your token a name and copy it (don’t lose it because 
+#it will never appear again!).
+# Token_name : M2_CRC_Immunex_master_project_I2P 
+# Token : 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
+gitcreds::gitcreds_set()
