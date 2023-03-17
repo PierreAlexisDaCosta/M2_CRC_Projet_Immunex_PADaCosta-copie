511a9b1e210bda6c7022a3b65a4b0daeed0b17cc
PierreAlexisDaCosta <pierrealexis.da.costa@gmail.com>
2023-03-17 11:42:50 +0100

    Initial commit

--- Script/Packages.R
+++ Script/Packages.R
@@ -41,18 +41,18 @@ library(janitor)
 library(RColorBrewer)
 
 # github configuration ####
-#library(usethis)
-#library(gitcreds)
-#usethis::edit_git_config() #Veryfy username and mail
-#usethis::use_git()
-#usethis::create_github_token()
+library(usethis)
+library(gitcreds)
+usethis::edit_git_config() #Veryfy username and mail
+usethis::use_git()
+usethis::create_github_token()
 #This will take you to the appropriate page on the GitHub website, 
 #where you’ll give your token a name and copy it (don’t lose it because 
 #it will never appear again!).
 # Token_name : M2_CRC_Immunex_master_project_I2P 
 # Token : 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
-#gitcreds::gitcreds_set()
+gitcreds::gitcreds_set()
 
-#usethis::use_git()
-#usethis::use_github()
+usethis::use_git()
+usethis::use_github()
 
