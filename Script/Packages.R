471430fbbf676dd5433dc877a8fcc74973c02652
PierreAlexisDaCosta <pierrealexis.da.costa@gmail.com>
2023-04-05 18:23:18 +0200

    Initial commit

--- Script/Packages.R
+++ Script/Packages.R
@@ -41,19 +41,19 @@ library(janitor)
 library(RColorBrewer)
 
 # github configuration ####
-#library(usethis)
-#library(gitcreds)
-#usethis::edit_git_config() #Veryfy username and mail
-#usethis::use_git()
-#usethis::create_github_token()
-#This will take you to the appropriate page on the GitHub website, 
-#where you’ll give your token a name and copy it (don’t lose it because 
-#it will never appear again!).
-# Token_name : M2_CRC_Immunex_master_project_I2P 
-# Token : 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
-#gitcreds::gitcreds_set()
+library(usethis)
+library(gitcreds)
+usethis::edit_git_config() #Veryfy username and mail
+usethis::use_git()
+usethis::create_github_token()
+# This will take you to the appropriate page on the GitHub website,
+# where you’ll give your token a name and copy it (don’t lose it because
+# it will never appear again!).
+# Token_name : M2_CRC_Immunex_master_project_I2P
+# Token : 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
+gitcreds::gitcreds_set()
 
-#usethis::use_git()
-#usethis::use_github()
-#usethis::use_git_remote("origin", url = NULL, overwrite = TRUE)
+usethis::use_git()
+usethis::use_github()
+usethis::use_git_remote("origin", url = NULL, overwrite = TRUE)
 
